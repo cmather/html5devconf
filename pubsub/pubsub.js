d4ef44692b236ec326a706f2961444a540c9e131
Chris Mather <mather.chris@gmail.com>
2014-10-22 13:07:13 -0700

    add some rendered items

--- pubsub/pubsub.js
+++ pubsub/pubsub.js
@@ -1,4 +1,11 @@
 if (Meteor.isClient) {
+  Template.Items.helpers({
+    items: function () {
+      return Items.find({},{sort:{order:1}});
+    }
+  });
+
+  Meteor.subscribe('allItems');
 }
 
 if (Meteor.isServer) {
